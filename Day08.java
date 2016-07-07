9bc93c2ac21e344c6f9b62f71c6c91de532a360f
Charmy Garg <charmiigarg@gmail.com>
2016-07-07 11:59:27 +0530

    Day 1 to Day 14 Code

---
+++ Day08.java
@@ -0,0 +1,31 @@
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DictionaryPractice {
+    public static void main(String[] args) {
+
+        Map<String, Integer> dict  =  new HashMap<>();
+
+        Scanner s1 = new Scanner(System.in);
+        int N =  s1.nextInt();
+
+        for (int i =0 ; i<N ; i++ ){
+            String name = s1.next();
+            int number = s1.nextInt();
+            dict.put(name, number);
+
+        }
+ 
+        for(int i = 0 ; i<N ; i++){
+            String givenName = s1.next();
+            if(dict.containsKey(givenName)){
+                System.out.println(givenName+ "="+ dict.get(givenName));
+            }else{
+                System.out.println("Not found");
+            }
+        }
+
+    }
+}
